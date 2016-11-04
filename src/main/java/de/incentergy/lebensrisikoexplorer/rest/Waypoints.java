996411fce3b7039e4dca80fedbb110ee26e53d13
Manuel Blechschmidt <manuel.blechschmidt@gmx.de>
2016-11-04 23:00:12 +0100

    * Added crossfilter

--- src/main/java/de/incentergy/lebensrisikoexplorer/rest/Waypoints.java
+++ src/main/java/de/incentergy/lebensrisikoexplorer/rest/Waypoints.java
@@ -1,9 +1,12 @@
 package de.incentergy.lebensrisikoexplorer.rest;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("waypoints")
@@ -14,6 +17,12 @@ public class Waypoints {
 
 	@GET
 	public Response get() {
-		return Response.ok().entity("Hallo Welt").build();
+
+		List<?> rows = em
+				.createNativeQuery(
+						"select COUNT(*),  date\\:\\:timestamp\\:\\:date FROM waypoint WHERE EXTRACT(year FROM date) = 2015 GROUP BY date\\:\\:timestamp\\:\\:date ORDER BY date\\:\\:timestamp\\:\\:date;")
+				.getResultList();
+
+		return Response.ok().entity(rows).type(MediaType.APPLICATION_JSON).build();
 	}
 }
--- src/main/sql/Queries.sql
+++ src/main/sql/Queries.sql
@@ -6,5 +6,12 @@ select COUNT(*),  EXTRACT(year FROM date) as year, EXTRACT(month FROM date) as m
 
 select COUNT(*),  EXTRACT(year FROM date) as year, EXTRACT(month FROM date) as month, EXTRACT(day FROM date) as day FROM waypoint WHERE EXTRACT(year FROM date) = 2015 GROUP BY year, month, day ORDER BY month;
 
+select COUNT(*) as count,  EXTRACT(year FROM date) as year, EXTRACT(month FROM date) as month, EXTRACT(day FROM date) as day FROM waypoint WHERE EXTRACT(year FROM date) = 2015 GROUP BY year, month, day HAVING COUNT(*) < 240 ORDER BY month;
+
+select COUNT(*) as count,  EXTRACT(year FROM date) as year, EXTRACT(month FROM date) as month, EXTRACT(day FROM date) as day FROM waypoint WHERE EXTRACT(year FROM date) = 2015 GROUP BY year, month, day HAVING COUNT(*) > 1500 ORDER BY month;
+
+
+select COUNT(*),  date::timestamp::date FROM waypoint WHERE EXTRACT(year FROM date) = 2015 GROUP BY date::timestamp::date ORDER BY date::timestamp::date;
+
 
 select COUNT(*) as count, name FROM waypoint, region WHERE ST_WITHIN(point, geometry) GROUP BY name ORDER BY count DESC;
