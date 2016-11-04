0ccd9a8b699db5e187ba26416bf72ff6b4f7b8c3
Manuel Blechschmidt <manuel.blechschmidt@gmx.de>
2016-11-04 21:16:30 +0100

    * Commitet some data samples

---
+++ src/main/sql/Queries.sql
@@ -0,0 +1,10 @@
+﻿select COUNT(*) FROM waypoint;
+select COUNT(*),  EXTRACT(year FROM date) as year FROM waypoint GROUP BY year;
+
+
+select COUNT(*),  EXTRACT(year FROM date) as year, EXTRACT(month FROM date) as month FROM waypoint WHERE EXTRACT(year FROM date) = 2015 GROUP BY year, month ORDER BY month;
+
+select COUNT(*),  EXTRACT(year FROM date) as year, EXTRACT(month FROM date) as month, EXTRACT(day FROM date) as day FROM waypoint WHERE EXTRACT(year FROM date) = 2015 GROUP BY year, month, day ORDER BY month;
+
+
+select COUNT(*) as count, name FROM waypoint, region WHERE ST_WITHIN(point, geometry) GROUP BY name ORDER BY count DESC;
